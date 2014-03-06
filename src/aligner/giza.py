d1d1e3e513f5cd106e3b865caee118d177b93137
Vladimir Zaytsev <zvm@guest-wireless-nup-nat-206-117-89-004.usc.edu>
2014-03-06 15:18:37 -0800

    Add DuraniPP Heuristic

---
+++ src/aligner/giza.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Author: Vladimir M. Zaytsev <zaytsev@usc.edu>
+
+class GizaAlignmentReader(object):
+    """
+    This class is responsible for reading GIZA++ aliglignment (A3) files.
+    A3 file format is described here: http://goo.gl/nFFvE8
+    """
+    def __init__(self, a3_stream):
+        pass
+
+    def __iter__(self):
+        pass
