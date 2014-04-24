b8f1956e8703fc65e3eee4a15197e6b5d47dd07d
Vova Zaytsev <zaytsev@usc.edu>
2014-04-23 23:04:14 -0700

    Update aligner

--- sbin/run-aligner.sh
+++ sbin/run-aligner.sh
@@ -6,5 +6,5 @@ $PYTHON $ROOT/nnsmt/aligner.py     \
     --source-text $TRAIN_SRC_TEXT  \
     --target-text $TRAIN_TRG_TEXT  \
     --alignment   $TRAIN_ALIGNMENT \
-    --verbosity-level 1            #\
-    # > $WORK_DIR/pp-alignment.txt
+    --verbosity-level 1            \
+    > $WORK_DIR/pp-alignment.txt
