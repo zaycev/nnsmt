cf745587da90a0eaa2b107562053c702a52ea57c
Vova Zaytsev <zaytsev@usc.edu>
2014-04-23 19:02:40 -0700

    Add example data

--- sbin/run-env.sh
+++ sbin/run-env.sh
@@ -3,14 +3,17 @@
 THIS=`pwd`
 ROOT=$THIS/..
 NPLM_ROOT=$ROOT/nplm
-DATA_ROOT=/Users/zvm/dev/dr-data/nnsmt
+DATA_ROOT=$ROOT/contrib
 
+# Training data
 TRAIN_SRC_TEXT=$DATA_ROOT/train.de.txt
 TRAIN_TRG_TEXT=$DATA_ROOT/train.de.txt
 TRAIN_ALIGMENT=$DATA_ROOT/train.a3.txt
 
+# Development data
 DEV_SRC_TEXT=$DATA_ROOT/train.de.txt
 DEV_TRG_TEXT=$DATA_ROOT/train.de.txt
 
+# Tests data
 TEST_SRC_TEXT=$DATA_ROOT/train.de.txt
 TEST_TRG_TEXT=$DATA_ROOT/train.de.txt
