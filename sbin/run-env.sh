68d070ea4520a66fcf1271b12b38c213c5de2249
Vova Zaytsev <zaytsev@usc.edu>
2014-04-23 19:01:27 -0700

    Remove old junk code

--- sbin/run-env.sh
+++ sbin/run-env.sh
@@ -1,5 +1,16 @@
 # /usr/bin/env bash
 
-THIS_DIR=`pwd`
-ROOT_DIR=$THIS_DIR/..
-NPLM_ROOT=$ROOT_DIR/nplm
+THIS=`pwd`
+ROOT=$THIS/..
+NPLM_ROOT=$ROOT/nplm
+DATA_ROOT=/Users/zvm/dev/dr-data/nnsmt
+
+TRAIN_SRC_TEXT=$DATA_ROOT/train.de.txt
+TRAIN_TRG_TEXT=$DATA_ROOT/train.de.txt
+TRAIN_ALIGMENT=$DATA_ROOT/train.a3.txt
+
+DEV_SRC_TEXT=$DATA_ROOT/train.de.txt
+DEV_TRG_TEXT=$DATA_ROOT/train.de.txt
+
+TEST_SRC_TEXT=$DATA_ROOT/train.de.txt
+TEST_TRG_TEXT=$DATA_ROOT/train.de.txt
