f096ef8cf016e2a7d8f65b1170b5bf8cd822f2dd
Vova Zaytsev <zaytsev@usc.edu>
2014-04-23 20:05:26 -0700

    Upate build script

--- nnsmt/aligner.py
+++ nnsmt/aligner.py
@@ -1,39 +1,50 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Author: Vladimir M. Zaytsev <zaytsev@usc.edu>
+# Author: Vova Zaytsev <zaytsev@usc.edu>
 
 """
 """
 
 import sys
-import giza
 import logging
-import argparse
 import heuristics
 
 
+class AffilationHeuristicAligner(object):
+    """
+    """
+    
+    def __ini__(self):
+        pass
+
+
+
 HEURISCICS = {
-    "durani-pp": heuristics.DuraniPPHeuristic, #
+    "affiliation": AffilationHeuristicAligner
 }
+
+DEFAULT_HEURISTIC = HEURISCICS.keys()[0]
+
 
 
 if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-a3", "--giza-a3-file",      type=str)
-    argparser.add_argument("-sv", "--source-vocabulary", type=str)
-    argparser.add_argument("-tv", "--target-vocabulary", type=str)
-    argparser.add_argument("-o",  "--output-path",       type=str)
-    argparser.add_argument("-h",  "--heuristic",         type=str, default="durani-pp", choices=HEURISCICS.keys())
-    argparser.add_argument("-v",  "--verbosity-level",   type=int, default=1,           choices=(0, 1, 2))
+    argparser.add_argument("-a", "--alignment",         type=str)
+    argparser.add_argument("-s", "--source-text",       type=str)
+    argparser.add_argument("-t", "--target-text",       type=str)
+    argparser.add_argument("-g", "--heuristic",         type=str,
+                                                        default=DEFAULT_HEURISTIC,
+                                                        choices=HEURISCICS.keys())
+    argparser.add_argument("-v", "--verbosity-level",   type=int, default=1, choices=(0, 1, 2))
     arguments = arg_parser.parse_args()
 
     if arguments.verbosity_level > 0:
         logging.basicConfig(level=logging.INFO)
 
-    a3_file = open(arguments.giza_a3_file, "rb")
-    s_vcb = giza.load_vcb(arguments.source_vocabulary)
-    t_vcb = giza.load_vcb(arguments.target_vocabulary)
+    a_file = open(arguments.alignment,   "rb")
+    s_file = open(arguments.source_text, "rb")
+    t_file = open(arguments.target_text, "rb")
 
     a_reader = giza.GizaAlignmentReader(a3_file)
 
--- sbin/run-build.sh
+++ sbin/run-build.sh
@@ -2,6 +2,22 @@
 
 source run-env.sh
 
-cd $NPLM_ROOT
+mkdir -p $ROOT/build/bin/
+mkdir -p $ROOT/build/python/
+
+cd $NPLM_ROOT/src
 
 make
+make python/nplm.so
+
+mv $NPLM_ROOT/src/neuralLM.so          $ROOT/build/bin/
+mv $NPLM_ROOT/src/neuralLM.a           $ROOT/build/bin/
+mv $NPLM_ROOT/src/prepareNeuralLM      $ROOT/build/bin/
+mv $NPLM_ROOT/src/prepareNeuralTM      $ROOT/build/bin/
+mv $NPLM_ROOT/src/testNeuralLM         $ROOT/build/bin/
+mv $NPLM_ROOT/src/testNeuralNetwork    $ROOT/build/bin/
+mv $NPLM_ROOT/src/trainNeuralNetwork   $ROOT/build/bin/
+
+mv $NPLM_ROOT/src/python/nplm.pxd      $ROOT/build/python/
+mv $NPLM_ROOT/src/python/nplm.pyx      $ROOT/build/python/
+mv $NPLM_ROOT/src/python/nplm.so       $ROOT/build/python/
